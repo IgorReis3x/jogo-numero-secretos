653726a71ebb01fbf248db0a51d7140544cb6239
Igor Reis <reis66891@gmail.com>
2025-08-14 23:03:56 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,73 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 100
+let numeroSecreto = gerarNumeroAletorio();
+let tentativas = 1;
+// manipulando o html com o uso do JS
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+     if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do Numero Secreto');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100 (ᶜʳᵉᵃᵗᵉᵈ ᵇʸ ˡᵍᵒʳ)');
+}
+   
+exibirMensagemInicial();
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTetativa = tentativas > 1 ? 'tetantivas' : 'tentativa';
+        let mensagemTentativas = `voçe descobriu o numero secreto com ${tentativas} ${palavraTetativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', ' O numero secreto e menor');
+        } else {
+            exibirTextoNaTela('p', 'O numero secreto e maior');
+        }
+        tentativas++;
+    }   limparCampo();
+}
+
+function gerarNumeroAletorio() {
+    let NumeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLiasta = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLiasta == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(NumeroEscolhido)) {
+        return gerarNumeroAletorio();
+    } else {
+        listaDeNumerosSorteados.push(NumeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return NumeroEscolhido;
+    }
+}
+
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAletorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+}
